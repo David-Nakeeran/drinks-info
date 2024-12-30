82e64280f66490f2cb115e332ef167731679598f
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-12-30 13:27:07 +0000

    First commit

---
+++ DrinksInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace DrinksInfo;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        // DrinksService drinksService = new DrinksService();
+        // var categories = await drinksService.GetDrinksCategoriesAsync();
+        // // Check if categories were returned
+        // if (categories.Any())
+        // {
+        //     foreach (var category in categories)
+        //     {
+        //         Console.WriteLine(category.Name);
+        //     }
+        // }
+        // else
+        // {
+        //     Console.WriteLine("No categories found.");
+        // }
+
+
+    }
+}
+
