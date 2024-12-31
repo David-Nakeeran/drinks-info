39c8753fa7e445b8036474ad631bc0d71a2aeeba
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-12-31 13:42:02 +0000

    Created object instances - Drinks Services - Menu Handler - Validation - Drinks Controller
    Currently get List of drink categories from API
    ShowCategoryMenu called to down list to user

--- DrinksInfo/Program.cs
+++ DrinksInfo/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using DrinksInfo.Utilities;
+using DrinksInfo.Controllers;
+using DrinksInfo.Views;
 
 namespace DrinksInfo;
 
@@ -6,8 +8,12 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        // DrinksService drinksService = new DrinksService();
-        // var categories = await drinksService.GetDrinksCategoriesAsync();
+        DrinksService drinksService = new DrinksService();
+        MenuHandler menuHandler = new MenuHandler();
+        Validation validation = new Validation();
+        DrinksControllers drinksControllers = new DrinksControllers(validation);
+        var categories = await drinksService.GetDrinksCategoriesAsync();
+        menuHandler.ShowCategoryMenu(categories);
         // // Check if categories were returned
         // if (categories.Any())
         // {
