417194f55e49d798f6e886ac118895a49fe61e17
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-12-31 13:50:18 +0000

    GetCategory method - In progress     - Will get user input     - Validate input     - return input

---
+++ DrinksInfo/Controllers/DrinksController.cs
@@ -0,0 +1,18 @@
+
+
+using DrinksInfo.Utilities;
+
+namespace DrinksInfo.Controllers;
+
+class DrinksControllers
+{
+    private readonly Validation _validation;
+    internal DrinksControllers(Validation validation)
+    {
+        _validation = validation;
+    }
+    internal string? GetCategory()
+    {
+
+    }
+}
