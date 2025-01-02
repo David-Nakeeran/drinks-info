bff6118ae7ac20bfc80c2d8ad39ba60533a0ca8b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-02 12:40:21 +0000

    Added ShowDrinksMenu method      - Creates table from list

--- DrinksInfo/Views/MenuHandler.cs
+++ DrinksInfo/Views/MenuHandler.cs
@@ -25,4 +25,25 @@ class MenuHandler
         }
         AnsiConsole.Write(table);
     }
+
+    internal void ShowDrinksMenu(List<Drink> drinks)
+    {
+        var table = new Table();
+        table.AddColumn("Categories Menu");
+
+        // loop through add the rows
+        foreach (var drink in drinks)
+        {
+            if (drink.Name == null)
+            {
+                AnsiConsole.WriteLine("No categories");
+                return;
+            }
+            else
+            {
+                table.AddRow(drink.Name);
+            }
+        }
+        AnsiConsole.Write(table);
+    }
 }
