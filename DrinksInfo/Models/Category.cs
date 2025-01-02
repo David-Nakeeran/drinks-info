40b26d61a927b90d5113dc63d3fceec07ed57d3b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-02 12:34:23 +0000

    formatting

--- DrinksInfo/Models/Category.cs
+++ DrinksInfo/Models/Category.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace DrinksInfo.Models;
 
-internal class Category()
+internal class Category
 {
     [JsonPropertyName("strCategory")]
     public string? Name { get; set; }
