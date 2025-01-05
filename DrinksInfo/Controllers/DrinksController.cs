ec0133c28150e5e9094ab08672f166e7024a391d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-05 13:49:35 +0000

    Refactored code

--- DrinksInfo/Controllers/DrinksController.cs
+++ DrinksInfo/Controllers/DrinksController.cs
@@ -1,6 +1,3 @@
-
-
-using System.Reflection.Metadata.Ecma335;
 using DrinksInfo.Utilities;
 using Spectre.Console;
 
@@ -9,7 +6,7 @@ namespace DrinksInfo.Controllers;
 class DrinksControllers
 {
     private readonly Validation _validation;
-    internal DrinksControllers(Validation validation)
+    public DrinksControllers(Validation validation)
     {
         _validation = validation;
     }
@@ -17,6 +14,13 @@ class DrinksControllers
     {
         var input = AnsiConsole.Ask<string>("Please enter the category of drinks you'd like to see");
         var inputValid = _validation.ValidateString("Please try again, enter the category of drinks you'd like to see", input);
+        return inputValid;
+    }
+
+    internal string? GetDrinkDetail()
+    {
+        var input = AnsiConsole.Ask<string>("Please enter the drink id you'd like to view details of or enter 0 to return to all drink categories");
+        var inputValid = _validation.ValidateDrinkId("Please try again, enter the drink id you'd like to view details of", input);
         return inputValid;
     }
 }
